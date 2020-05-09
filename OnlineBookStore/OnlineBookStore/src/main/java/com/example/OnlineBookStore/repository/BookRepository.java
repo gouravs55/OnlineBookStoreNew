de743c4bc74918bb9e9c9a25c19fa854438dd090
Gourav Satish Sharma <Gourav Satish Sharma@192.168.1.10>
2020-05-09 12:39:31 +0530

    Third Commit upto 42 videos

--- OnlineBookStore/OnlineBookStore/src/main/java/com/example/OnlineBookStore/repository/BookRepository.java
+++ OnlineBookStore/OnlineBookStore/src/main/java/com/example/OnlineBookStore/repository/BookRepository.java
@@ -1,7 +1,10 @@
 package com.example.OnlineBookStore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.OnlineBookStore.entity.Book;
@@ -9,4 +12,8 @@ import com.example.OnlineBookStore.entity.Book;
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
+	//It changes the rest end point http://localhost:8080/api/v1/books/search/findByCategoryId?id=4
+	// By adding this @RestResource the restend point becomes http://localhost:8080/api/v1/books/search/categoryid?id=2
+	@RestResource(path="categoryid")
+	Page<Book> findByCategoryId(@Param("id")Long id,Pageable pageable);
 }
